36f0a2a300e2fdf8d68eb6493f7f84a9a7788f5b
SwordMaster <43595245+SwordMaster191@users.noreply.github.com>
2021-07-14 11:00:20 +0700

    added login page

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,8 +1,55 @@
 <template>
   <v-container>
-    <div>
-      <h1>This is for login page</h1>
-    </div>
-  </v-container>
+    <template>
+      <v-form ref="form" v-model="valid" lazy-validation>
+        <v-text-field
+          v-model="username"
+          :rules="usernameRules"
+          label="Username"
+          required
+        ></v-text-field>
 
+        <v-text-field
+          type="password"
+          v-model="password"
+          :rules="passwordRules"
+          label="Password"
+          required
+        ></v-text-field>
+
+        <v-btn
+          :disabled="!valid"
+          color="success"
+          class="mr-4"
+          @click="submit"
+        >
+          Login
+        </v-btn>
+
+        <v-btn color="error" class="mr-4" @click="reset"> Reset </v-btn>
+
+      </v-form>
+    </template>
+  </v-container>
 </template>
+<script>
+export default {
+  data: () => ({
+    valid: true,
+    username:'',
+    password:'',
+    usernameRules: [(v) => !!v || "Userame is required"],
+    passwordRules: [(v) => !!v || "Password is required"],
+  }),
+
+  methods: {
+    submit() {
+      this.$refs.form.validate();
+      console.log(this.usernameRules, this.password)
+    },
+    reset() {
+      this.$refs.form.reset();
+    },
+  },
+};
+</script>
